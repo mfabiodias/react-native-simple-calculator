f6a27c3c56dce38991a74b4d46d7747117b2b1c6
Fabio Dias <mfabiodias@gmail.com>
2020-01-14 21:28:52 -0300

    Layout da calculadora implementado

--- src/components/Button.js
+++ src/components/Button.js
@@ -16,13 +16,30 @@ const style = StyleSheet.create({
         borderWidth: 1,
         borderColor: '#888',
 
+    },
+    operationButton: {
+        color: '#fff',
+        backgroundColor: '#fa8231',
+    },
+    buttonDouble: {
+        width: Dimensions.get('window').width / 2,
+    },
+    buttonTriple: {
+        width: (Dimensions.get('window').width / 4) * 3,
     }
 })
 
 export default props => {
+
+    const StyleButton = [style.button]
+    
+    if(props.double) StyleButton.push(style.buttonDouble)
+    else if(props.triple) StyleButton.push(style.buttonTriple)
+    else if(props.operation) StyleButton.push(style.operationButton)
+    
     return (
-        <TouchableHighlight onPress={props.onClick} >
-            <Text style={style.button}>{props.label}</Text>
+        <TouchableHighlight onPress={() => props.onClick(props.label)} >
+            <Text style={StyleButton}>{props.label}</Text>
         </TouchableHighlight>
     )
 }
