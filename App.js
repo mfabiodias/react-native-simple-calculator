255726c30103272c53885a4c9ce32bc78b6ce36f
Fabio Dias <mfabiodias@gmail.com>
2020-01-16 14:20:24 -0300

    Adicionado todas as funcionalidades e layout da calculadora MacOS

--- App.js
+++ App.js
@@ -6,29 +6,10 @@ import Display from './src/components/Display'
 const initialState = {
   displayValue: '0', // Valor inicial do visor
   clearDisplay: false, // Atributo para limpar o display
-  operation: null, // Armazena o operador pressionado
+  operation: null, // Atributo que armazena o operador
   values: [0,0], // Explicação abaixo.
   current: 0, // Posição para adicionar valores no array values. Valores validos (0 e 1)
 }
-
-/**
- * initialState->values: Este array armazena os valores digitados pelo usuário em dois estágios. Exe:
- * 
- * Parte 1
- * A medida que o usuário pressiona os dígitos na calculadora (0~9 e .), estes são concatenados e armazenados em values[0].
- * 
- * Parte 2
- * No momento que o usuário pressiona um operador (+, -, *, /) o ponteiro do values passa de 0 para 1 através do atributo current.
- * Nesse momento values[0] tem um valor armazenado e o operador passa de null para o operador pressionado e o display é limpo.
- * 
- * Parte 3
- * Identico a Parte 1, os valores digitados são concatenados e armazenados em values[1]
- * 
- * Parte 4 
- * Similar a Parte 2, com a diferença que nesse momento podemos receber um novo operador (=) que irá apresentar o resultado. 
- * No caso de outro operador ser pressionado, o cálculo é realizado, armazenado em values[0], enquanto values[1] ficará no 
- * aguardo de um novo valor para aplicar a lógica do operador ou finalizar com o operador (=)
- */
 
 export default class APP extends Component {
 
@@ -58,6 +39,9 @@ export default class APP extends Component {
     // Validação para apenas um ponto. Caso o ponto já esteja em displayValue ele é ignorado 
     if(n === ',' && !clearDisplay && this.state.displayValue.includes(',')) return
 
+    // Insere o zero antes da vírgula quando número inteiro for menor que 1
+    if(n === ',' && clearDisplay) n = '0,'
+
     // Armazena valor corrente quando clearDisplay !== true 
     const currentValue = clearDisplay ? '' : this.state.displayValue
 
@@ -82,6 +66,41 @@ export default class APP extends Component {
     this.setState({ ...initialState })
   }
 
+  // Função realiza o total das operações
+  setTotal = (clearValue) => {
+    
+    const values = [... this.state.values]
+
+    // Opção para zerar o valor anterior da memória quando setTotal e invocada de setOperation
+    values[1] = clearValue === true ? 0 : values[1]
+
+    try{
+      // Eval retorna o resultado da operação setada anteriomente e armazena em values[0]
+      values[0] = parseFloat(eval(`${values[0]} ${this.state.operation} ${values[1]}`).toFixed(9));
+      
+      //Corrige o problema de divisão de 0/0 que retornava NaN 
+      values[0] = Number.isNaN(values[0]) ? 0 : values[0]
+    }
+    // Em caso de falha restaura para o valor anterior
+    catch(e){
+      //console.debug(e)
+      values[0] = this.state.values[0]
+    }
+
+    // Define o estado da calculadora após aplicar a lógica
+    if(clearValue === true)
+    {
+      this.setState({ values })
+    }
+    else
+    {
+      this.setState({
+        displayValue: this.getDisplayValue(values[0]),  
+        values,
+      })
+    }
+  }
+
   // Aplica a operação selecionada (Lógica da calculadora).
   setOperation = operation => {
 
@@ -96,19 +115,19 @@ export default class APP extends Component {
       // Implementamos a lógica da função que muda o sinal do número recebido
       values[current] = operation === '+/-' ? this.reverseSign(values[current]) 
         // Implementada a lógica da porcentagem
-        : values[1] == 0 ? values[current] / 100 : values[0] * values[1] / 100
+        : current == 0 ? values[current] / 100 : values[0] * values[1] / 100
 
       // Define o estado da calculadora após aplicar a lógica
       this.setState({
         displayValue: this.getDisplayValue(values[current]), // Seta o resultado da operação como string para o display 
         operation: (operation === '%' && values[1] == 0) ? null : this.state.operation, // Seta ou redefine o atributo
-        current: current, // Salva a posição do array values
-        clearDisplay: true, // Limpa o display quando digitado os operadores (+, -, *, /)
-        values, // Armasena o array atual no estado
+        current, // Salva a posição do array values
+        values, // Armazena o array atual no estado
       })
     }
     else
     {
+
       // Armazena o operador e incrementa o ponteiro de values através do atributo current
       if(current === 0)
       {
@@ -117,69 +136,12 @@ export default class APP extends Component {
       // Aplica a lógica ao operador armazenado na etapa anterior no array values
       else
       {
-        // Inicia constantes para aplicação da lógica
-        const equals = operation === '='
-        //const values = [... this.state.values]
-
-        // Eval retorna o resultado da operação setada anteriomente e armazena em values[0]
-        try{
-          // console.debug(eval(`${values[0]} ${this.state.operation} ${values[1]}`));
-          // console.debug(parseFloat(eval(`${values[0]} ${this.state.operation} ${values[1]}`).toFixed(9)));
-          
-          /**
-           * #############################
-           * ######## Problema #1 ########
-           * #############################
-           * 
-           * Ao realizar soma ou subtração com números decimais o javascript apresenta problema na precisão.
-           * 
-           * *** Deprecated ***
-           * values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`)
-           * 
-           * Isso ocorre porque os valores são transformados em strings para utilização na função eval.
-           * O Javascript tem esse problema de precisão quando realiza somas de strings. Esse probela 
-           * poderá ser testado no console de qualquer navegador.
-           * 
-           * Exe #1: 
-           * '5.3' + '0.6' = 5.8999999999999995
-           * 5.3 + 0.6 = 5.9
-           * 
-           * Exe #2: 
-           * '5.6' - '0.2' = 5.3999999999999995 
-           * 5.6 - 0.2 = 5.4 
-           * 
-           */
-
-          /**
-           * #############################
-           * ######## Melhoria #1 ########
-           * #############################
-           * 
-           * O Problema #1 foi solicionado com a adição do toFixed(9) para precisão com a quantidade de dígitos 
-           * possíveis no visor. 
-           *  
-           * (5.3 + 0.6).toFixed(9) = "5.900000000"
-           * 
-           * E posteriormente a introdução do parseFloat() que irá ignorar os zeros a direita retornando: 
-           * parseFloat("5.900000000") = 5.9
-           */
-
-          values[0] = parseFloat(eval(`${values[0]} ${this.state.operation} ${values[1]}`).toFixed(9));
-        }
-        // Em caso de falha restaura para o valor anterior
-        catch(e){
-          console.debug(e)
-          values[0] = this.state.values[0]
-        }
-
-        // Zera values[1] após aplicação do operador acima, estando apto a receber nova sequencia de digitos
-        values[1] = 0
+        // Zera o values[1] quando o usuário clica em uma operação (+, -, *, /, %, +/-) e values[1] não está zerada
+        this.setTotal(true)
 
         // Define o estado da calculadora após aplicar a lógica
         this.setState({
-          displayValue: this.getDisplayValue(values[0]), // Seta o resultado da operação como string para o display 
-          operation: equals ? null : operation, // Seta ou redefine o atributo
-          current: equals ? 0 : 1, // Salva a posição do array values
+          operation: operation, // Seta ou redefine o atributo
           clearDisplay: true, // Limpa o display quando digitado os operadores (+, -, *, /)
           values, // Armasena o array atual no estado
         })
@@ -192,9 +154,9 @@ export default class APP extends Component {
       <View style={styles.container}>
         <Display value={this.state.displayValue} />
         <View style={styles.buttons}>
-          <Button label='AC'  onClick={this.clearMemory} ></Button>
-          <Button label='+/-' onClick={this.setOperation} ></Button>
-          <Button label='%'   onClick={this.setOperation} ></Button>
+          <Button label='AC'  onClick={this.clearMemory} top ></Button>
+          <Button label='+/-' onClick={this.setOperation} top ></Button>
+          <Button label='%'   onClick={this.setOperation} top ></Button>
           <Button label='÷'   onClick={this.setOperation} operation ></Button>
           <Button label='7'   onClick={this.addDigit} ></Button>
           <Button label='8'   onClick={this.addDigit} ></Button>
@@ -210,7 +172,7 @@ export default class APP extends Component {
           <Button label='+'   onClick={this.setOperation} operation ></Button>
           <Button label='0'   onClick={this.addDigit} double ></Button>
           <Button label=','   onClick={this.addDigit} ></Button>
-          <Button label='='   onClick={this.setOperation} operation ></Button>
+          <Button label='='   onClick={this.setTotal} operation ></Button>
         </View>
       </View>
     )
